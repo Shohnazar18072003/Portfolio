148fbadab885c5b3caef56567df9acaa47c154b5
Shohnazar2003 <129948756+Shohnazar2003@users.noreply.github.com>
2024-03-25 19:00:49 +0500

    Bug fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'm
-                <strong className="main-name">Shohnazar Abdusalomov</strong>
+                <strong className="main-name">  Shohnazar Abdusalomov</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
