b442b931825a07affdfd9633ff7ba7e8502d0447
Shohnazar2003 <129948756+Shohnazar2003@users.noreply.github.com>
2024-04-08 03:36:48 +0500

    Resume updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,7 +40,9 @@ function NavBar() {
       <Container>
         <Navbar.Brand href="/" className="d-flex">
           {/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
-          <h3 className="purple"><strong>Shohnazar</strong></h3>
+          <h3 className="purple">
+            <strong>Shohnazar</strong>
+          </h3>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
